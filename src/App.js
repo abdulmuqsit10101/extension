7fe8f4a1bd3c4d6859f9a0f0d6117c1390109cbf
abdulmuqsit1 <abdulmuqsit10101@gmail.com>
2019-09-19 21:11:42 +0500

    cleaning App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-    const data = useState('hhaa');
 
-
-    useEffect(() => {
-        console.log("I am here loaded! and Data : ", data);
-
-        function myFunction() {
-            var arr = [];
-
-            const x = document.querySelectorAll(".user-details a");
-            var i;
-            for (i = 0; i < x.length; i++) {
-                arr = arr.concat(x[i].innerHTML);
-            }
-            console.log("arr is  : ", arr);
-        }
-
-        myFunction();
-
-
-        // var mutationObserver = new MutationObserver(function () {
-        //     return myFunction();
-        // });
-        //
-        // mutationObserver.observe(document.querySelector('body'), {
-        //     attributes: true,
-        //     characterData: true,
-        //     childList: true,
-        //     subtree: true,
-        //     attributeOldValue: true,
-        //     characterDataOldValue: true
-        // });
-
-    });
     return (
         <div className="App">
         </div>
